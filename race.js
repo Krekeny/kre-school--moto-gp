3261cd80b55e52c6942f94963efe5a5c05bfc8d8
Sam Sauer <sam.sauer@krekeny.com>
2023-09-15 15:23:13 +0200

    refactor(race): update racetrack

--- race.js
+++ race.js
@@ -1,35 +1,40 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const canvas = document.getElementById("raceCanvas");
-//   const ctx = canvas.getContext("2d");
+let canvas = document.getElementById("raceCanvas");
+let ctx = canvas.getContext("2d");
 
-//   // Coordinates to keep track of each bike's position
-//   let bike1X = 0;
-//   let bike2X = 0;
+// Draw the track with rounded corners
+// ctx.lineWidth = 5;
+// ctx.beginPath();
+// ctx.moveTo(100, 20);
+// // line 1
+// ctx.lineTo(200, 160);
+// ctx.strokeStyle = "red";
+// ctx.stroke();
+// // quadratic curve
+// ctx.beginPath();
+// ctx.moveTo(200, 160);
+// ctx.quadraticCurveTo(230, 200, 260, 100);
+// ctx.strokeStyle = "blue";
+// ctx.stroke();
+// // bezier curve
+// ctx.beginPath();
+// ctx.moveTo(260, 100);
+// ctx.bezierCurveTo(290, -40, 300, 190, 400, 150);
+// ctx.strokeStyle = "green";
+// ctx.stroke();
+// ctx.closePath();
 
-//   document.getElementById("startRace").addEventListener("click", function () {
-//     // Implement the logic to start the race
-//     let raceInterval = setInterval(() => {
-//       // Clear the canvas
-//       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//       // Draw the bikes again at their new positions
-//       drawBike(ctx, bike1X, 50);
-//       drawBike(ctx, bike2X, 200);
-
-//       // Update the bike positions (you could use actual speed metrics here)
-//       bike1X += 5;
-//       bike2X += 4;
-
-//       // Stop the race when a bike reaches the end
-//       if (bike1X >= canvas.width || bike2X >= canvas.width) {
-//         clearInterval(raceInterval);
-//       }
-//     }, 100);
-//   });
-
-//   function drawBike(ctx, x, y) {
-//     // Sample code to draw a simple bike shape, you can replace this with an image or more complex drawing
-//     ctx.fillStyle = "blue";
-//     ctx.fillRect(x, y, 50, 25);
-//   }
-// });
+// Draw a rectangle with rounded corners
+// ctx.beginPath();
+// ctx.moveTo(100, 50);
+// ctx.lineTo(500, 50);
+// ctx.quadraticCurveTo(500, 50, 350, 100);
+// ctx.lineTo(350, 200);
+// ctx.quadraticCurveTo(350, 250, 300, 250);
+// ctx.lineTo(100, 250);
+// ctx.quadraticCurveTo(50, 250, 50, 200);
+// ctx.lineTo(50, 100);
+// ctx.quadraticCurveTo(50, 50, 100, 50);
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = "grey";
+// ctx.stroke();
+// ctx.closePath();
